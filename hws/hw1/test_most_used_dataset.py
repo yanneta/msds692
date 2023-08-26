fbac02fa10b8ce594214c85d9cb38f8a3bd878a5
yanneta <yannet@gmail.com>
2023-08-26 10:26:28 -0700

    fixed the test

--- hws/hw1/test_most_used_dataset.py
+++ hws/hw1/test_most_used_dataset.py
@@ -5,4 +5,4 @@ def test_list_of_pairs():
     for i in [5, 25, 41]:
         assert len(list_of_pairs(i)) == i
     info = list_of_pairs(43)
-    assert info[-1][0] == 'NYPD Shooting Incident Data (Historic)'
+    assert info[-1][0] == 'Harmonized Tariff Schedule of the United States (2023)'
