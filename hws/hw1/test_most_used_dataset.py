86d13439f5f4da8455891e0f76f0dab1293ba18c
yanneta <yannet@gmail.com>
2024-08-06 15:32:47 -0700

    updaing test

--- hws/hw1/test_most_used_dataset.py
+++ hws/hw1/test_most_used_dataset.py
@@ -1,8 +1,8 @@
-from most_used_dataset import *
+from most_used_dataset import list_of_pairs
 
 
 def test_list_of_pairs():
     for i in [5, 25, 41]:
         assert len(list_of_pairs(i)) == i
     info = list_of_pairs(43)
-    assert info[-1][0] == 'Harmonized Tariff Schedule of the United States (2023)'
+    assert info[3][0] == 'Crime Data from 2020 to Present'
